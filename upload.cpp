4aac5b84c80b0a12373ed3e093d4116ea4b7bd3e
taigo <tss@icomp.ufam.edu.br>
2018-10-06 22:33:24 -0400

    [UPD] usando strcpy

--- upload.cpp
+++ upload.cpp
@@ -9,7 +9,7 @@
 using  namespace std;
 
 int main (int argc, char** argv){
-    ifstream file("C:\\Users\\Katiely\\Documents\\UFAM\\6periodo\\BD\\tp2\\TP2-BD-TREE\\artigo.csv");
+    ifstream file(argv[1]);
     if(!file.is_open()){
         cout<<"ERROR CANT OPEN THE FILE"<<endl;
     }
@@ -30,45 +30,50 @@ int main (int argc, char** argv){
                 data.push_back(cell);
             }
         }
-        string title ;
-        title = data[1].c_str();
-        char *charTitle = (char*) malloc(sizeof(char) * MAX_SIZE_TITLE);
-       // charTitle = "";
-        charTitle = const_cast<char *>(data[1].c_str());
-       // char *charTitle = data[1];
-        auto *charAutorlist = (char*) malloc(sizeof(char) * MAX_SIZE_AUTOR);
-        charAutorlist = const_cast<char *>(data[3].c_str());
+        try {
 
-        auto *vSnipped = (char*) malloc(sizeof(char) * MAX_SIZE_SNIPPET);
-        vSnipped = const_cast<char *>(data[6].c_str());
+            string title;
+            title = data[1];
+            char charTitle[MAX_SIZE_TITLE]; // titulo
+            strcpy(charTitle, title.c_str());
+
+            char charAutorlist[MAX_SIZE_AUTOR]; // lista de autores
+            auto autorList = data[3];
+            strcpy(charAutorlist, autorList.c_str());
+
+            char vSnipped[MAX_SIZE_SNIPPET]; // resumo
+            auto snipet = data[6];
+            strcpy(vSnipped, snipet.c_str());
 
 
-        auto *vDate = (char*) malloc(sizeof(char) * MAX_SIZE_DATE);
-        vDate = const_cast<char *>(data[5].c_str());
+            char vDate[MAX_SIZE_DATE]; // data
+            auto date = data[5];
+            strcpy(vDate, date.c_str());
 
-        if (data[0].size()<1){
-            data[0] = totalReg;
-           // cout<<totalReg<<endl;
+//        if (data[0].size()<1){
+//            data[0] = totalReg;
+//           // cout<<totalReg<<endl;
+//
+//        }
+//        if(data[2].size()<3){
+//            data[2] = "0";
+//        }
+//        if(data[4].size()<18){
+//            data[4] = "0";
+//        }
 
+            int mID = stoi(data[0].c_str());
+            unsigned short int mYE = stoi(data[2].c_str());
+            int mQ = stoi(data[4].c_str());
+//            data.clear();
+            totalReg++;
+            //Article::Article(int id_, unsigned short int year_, int quote, char title[MAX_SIZE_TITLE] , char autorList[MAX_SIZE_AUTOR] , char snippet[MAX_SIZE_SNIPPET] , char date[ MAX_SIZE_DATE] ) {
+            Article *aux = new Article(totalReg, mYE, mQ, charTitle, charAutorlist, vSnipped, vDate);
+            cout << aux->toString();
+//        cout<<"---------"<<title<<endl;
+        }catch (exception &e){
+            cout << e.what() << endl;
         }
-        if(data[2].size()<3){
-            data[2] = "0";
-        }
-        if(data[4].size()<18){
-            data[4] = "0";
-        }
-        int mID = stoi(data[0].c_str());
-        unsigned short int mYE = stoi(data[2].c_str());
-        int mQ = stoi(data[4].c_str());
-        data.clear();
-        totalReg++;
-        //Article::Article(int id_, unsigned short int year_, int quote, char title[MAX_SIZE_TITLE] , char autorList[MAX_SIZE_AUTOR] , char snippet[MAX_SIZE_SNIPPET] , char date[ MAX_SIZE_DATE] ) {
-        Article *aux =  new Article(totalReg,mYE,mQ,charTitle, charAutorlist,vSnipped,vDate);
-        //cout<<aux->toString();
-        cout<<"---------"<<title<<endl;
-
-
-
 
     }
     cout<< "TOTAL : " << totalReg <<endl;
