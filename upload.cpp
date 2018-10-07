69b10d35ee516d5e10162e5ed3fcc2ba22f6a0ee
taigo <tss@icomp.ufam.edu.br>
2018-10-07 01:06:58 -0400

    [UPD] lendo todos os registros

--- upload.cpp
+++ upload.cpp
@@ -8,7 +8,10 @@
 #include <string.h>
 using  namespace std;
 
+
 int main (int argc, char** argv){
+//    Article *curArticle;
+
     ifstream file(argv[1]);
     if(!file.is_open()){
         cout<<"ERROR CANT OPEN THE FILE"<<endl;
@@ -17,68 +20,85 @@ int main (int argc, char** argv){
     int totalReg = 1;
     string snipped;
     //1.021.443
-    while(file.good() && totalReg<1021433) {
+    while(getline(file, snipped)) {
 
-        string line,dump,cell;
-        getline(file, snipped, '\n');
+        if (snipped.empty()){
+            continue;
+        }
+
+        string dump,cell;
+
         stringstream lineStream(snipped);
-
         vector<string> data;
         if (snipped.find(';') != std::string::npos) {
             while (std::getline(std::getline(lineStream, dump, '"'), cell, '"')) {
 //                cout << cell <<endl;
-                data.push_back(cell);
+                // tratando inconsistencias (nao sei explicar essa parte, foi tentativa e erro)
+                if(dump == ";;"){
+                    data.emplace_back("NULL");
+                    dump = ";";
+                    data.push_back(cell);
+                }else {
+                    if (data.size() >  1) {
+                        if (!cell.empty() && dump == ";") {
+                            data.push_back(cell);
+                        }
+                    } else {
+                        data.push_back(cell);
+                    }
+                }
+                // fim do tratamento de inconsistencias
             }
         }
         try {
 
+            size_t registerSize = data.size();
             string title;
-            title = data[1];
             char charTitle[MAX_SIZE_TITLE]; // titulo
-            strcpy(charTitle, title.c_str());
-
             char charAutorlist[MAX_SIZE_AUTOR]; // lista de autores
-            auto autorList = data[3];
-            strcpy(charAutorlist, autorList.c_str());
-
             char vSnipped[MAX_SIZE_SNIPPET]; // resumo
-            auto snipet = data[6];
-            strcpy(vSnipped, snipet.c_str());
-
-
             char vDate[MAX_SIZE_DATE]; // data
-            auto date = data[5];
-            strcpy(vDate, date.c_str());
+            int mQ; // citacoes
+            unsigned short mYE; // ano de publicacao
 
-//        if (data[0].size()<1){
-//            data[0] = totalReg;
-//           // cout<<totalReg<<endl;
-//
-//        }
-//        if(data[2].size()<3){
-//            data[2] = "0";
-//        }
-//        if(data[4].size()<18){
-//            data[4] = "0";
-//        }
+            if (registerSize > 4){
+                mQ = stoi(data[4].c_str());
+            }
 
-            int mID = stoi(data[0].c_str());
-            unsigned short int mYE = stoi(data[2].c_str());
-            int mQ = stoi(data[4].c_str());
-//            data.clear();
+            if (registerSize > 2){
+                mYE =  static_cast<unsigned short>(stoi(data[2].c_str()));
+            }
+
+            if (registerSize > 1) {
+                title = data[1].substr(0,MAX_SIZE_TITLE);
+                strcpy(charTitle, title.c_str());
+            }
+
+            if(registerSize > 3) {
+                auto autorList = data[3].substr(0,MAX_SIZE_AUTOR);
+                strcpy(charAutorlist, autorList.c_str());
+            }
+
+            if (data.size() > 6) {
+                auto snipet = data[6].substr(0,MAX_SIZE_SNIPPET);
+                strcpy(vSnipped, snipet.c_str());
+            }
+
+            if (registerSize > 5) {
+                auto date = data[5].substr(0,MAX_SIZE_DATE);
+                strcpy(vDate, date.c_str());
+            }
+
             totalReg++;
             //Article::Article(int id_, unsigned short int year_, int quote, char title[MAX_SIZE_TITLE] , char autorList[MAX_SIZE_AUTOR] , char snippet[MAX_SIZE_SNIPPET] , char date[ MAX_SIZE_DATE] ) {
-            Article *aux = new Article(totalReg, mYE, mQ, charTitle, charAutorlist, vSnipped, vDate);
-            cout << aux->toString();
-//        cout<<"---------"<<title<<endl;
-        }catch (exception &e){
-            cout << e.what() << endl;
+            Article current = Article(totalReg, mYE, mQ, charTitle, charAutorlist, vSnipped, vDate);
+            cout << current.toString();
+
+        }catch (const char *e){
+            throw e;
         }
-
     }
-    cout<< "TOTAL : " << totalReg <<endl;
-    file.close();
+    cout<< "\n\nTOTAL : " << totalReg <<endl;
     return 0;
-
 }
 
