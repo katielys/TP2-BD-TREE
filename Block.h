d9be9e4067850c3b4372dc49b2039c3c759fbd16
taigo <tss@icomp.ufam.edu.br>
2018-10-21 21:19:07 -0400

    [UPD] documentation

--- Block.h
+++ Block.h
@@ -1,4 +1,6 @@
 ///\file Block.h header from Block
+///\author Tiago Santos
+
 #include "Article.h"
 #ifndef TP2_BD_TREE_BLOCK_H
 #define TP2_BD_TREE_BLOCK_H
@@ -11,26 +13,54 @@ using namespace std;
 
 typedef unsigned char BYTE;
 
-struct Block{ // disk block representation
-    //header 13 bytes
-    unsigned int verificationMask;  // 4 bytes
-    unsigned int bytesOccupied;     // 4 bytes
-    unsigned int nextBlock;         // 4 bytes offset used as reference to find the overflow block
-    bool overflow;                  // 1 byte has overflow sequence or not
-    BYTE data[DATA_SIZE];           // array of bytes to keep the records
 
-    // tries to insert a record in the current block
+
+/**
+ *  Disk block representation, it's important to say that we've made this structure to serve as block and bucket
+ *  due the simplicity that it gave to us. This structure has a special attribute (nextBlock) that serves as pointer to
+ *  the next linked block, simulating the scheme of buckets and overflow blocks found in literature.
+ */
+struct Block{
+    /// header 13 bytes
+    unsigned int verificationMask;  //!< 4 bytes
+    unsigned int bytesOccupied;     //!< 4 bytes
+    unsigned int nextBlock;         //!< 4 bytes offset used as reference to find the overflow (or next) block
+    bool overflow;                  //!< 1 byte has overflow sequence or not
+    BYTE data[DATA_SIZE];           //!< array of bytes to keep the records
+
+    /**
+       * tries to insert a record in the current block
+       * @author Tiago Santos
+       * @param  article object to be inserted
+       * @return Returns a pair<bool, unsigned short>, the first value indicates if the
+       * record is inserted and the second indicates the logical position (0,1,2,..) of him inside the block
+       */
     pair<bool, unsigned short> insertRecord(const Article &article);
 
-    // returns the quantity of free space in a block
+    /// returns the quantity of free space in a block
+    /// @author Tiago Santos
     unsigned int getFreeSpace();
 
-    // search for a record with id passed as parameter, returns true if found or returns false if not found
+    /**
+       * search for a record with id passed as parameter, returns true if found or returns false if not found
+       * @author Tiago Santos
+       * @param id Is the identifier number of the record to be searched
+       * @param recordSize Is the size  of record that will be searched
+       * @param aux Article object to keep the data of the found record, only will be filled if found the record
+       * @return Returns a bool to indicate if found or not the record
+       */
     bool lookUpforRecord(unsigned int id, size_t recordSize, Article &aux);
 
+    /**
+       * search for a record by his offset
+       * @author Tiago Santos
+       * @param offset is the logical position of the record in a block, i.e. sequential position of record inside the block
+       * @param aux Article object to keep the data of the found record, only will be filled if found the record
+       */
     void lookUpforRecordByOffset(unsigned short offset, Article &aux);
 
-    // default constructor
+    /// default constructor
+    /// @author Tiago Santos
     Block();
 };
 
--- HashFile.h
+++ HashFile.h
@@ -1,3 +1,6 @@
+///\file HashFile.h Namespace that group all functions that manipulate the hashing file
+///\author Tiago Santos
+
 #ifndef TP2_BD_TREE_HASHFILE_H
 #define TP2_BD_TREE_HASHFILE_H
 #pragma once
@@ -6,47 +9,119 @@
 #include <fstream>
 #include "Parser.h"
 
-namespace Hashing {
+/** This namespace group all function that manipulate the hashing,
+ * it's important to note that in this work a buckets has the same size of a block (4096),
+ * thus the same structure is used as bucket. We decided to create two separated files, the first for the main hashing
+ * file that will contain the buckets, and the second to keep the overflow blocks. If a bucket in the main file needs
+ * a overflow block, a new overflow block will be allocated and the bucket will be updated to keep the address of his
+ * first overflow block. If needed overflow blocks can also keep address of subsequent overflow blocks, forming a
+ * logical linked list.
+*
+ **/
+ namespace Hashing {
 
-    const unsigned short HEADER_SIZE = sizeof(u_long);
+    /** Indicates the size of header, the header is summarily used
+    *   to keep tracking about the number of blocks allocated in hashing/overflow file
+    **/
+     const unsigned short HEADER_SIZE = sizeof(u_long);
 
+  /**
+   *  Struct address is the logical address of a record in the data file, indicates the exact location of an record and
+   *  is used in the index trees as "data pointers"
+   *  @author Tiago Santos
+   **/
     struct Address{
 
-        unsigned int offset;        // block offset of respective file (hashing|overflow)
-        unsigned short blockOffset; // offset of the record inside a block (can be 0 or 1).. change for a char
-        bool overflow = false;      // is in overflow area or not
+        unsigned int offset;        //!< block offset of respective file (hashing|overflow)
+        unsigned short blockOffset; //!< offset of the record inside a block (can be 0 or 1).. change for a char
+        bool overflow = false;      //!< is in overflow area or not
+
+        /// @author Tiago Santos
+        /// Default constructor
         Address();
     };
 
+    /// This struct is simply used to keep a instance of the overflow area, it keeps the file pointer an a counter of blocks inside the area
+    /// @author Tiago Santos
     struct OverflowArea{
-        FILE *fp;
-        unsigned blocksCount;
+        FILE *fp;             //!< file pointer to the overflow file
+        unsigned blocksCount; //!< counter of blocks in the overflow area
 
+        /// Default constructor
+        /// @author Tiago Santos
         OverflowArea();
 
+        /// Constructor that receives a path file and open the specified file, thus initializing the overflow area.
+        /// @author Tiago Santos
         explicit OverflowArea(const char *path);
 
+        /// Closes the file pointer of the overflow area
+        /// @author Tiago Santos
         void close();
 
     };
 
+    /// This struct is simply used to keep a instance of the hashing file, it keeps the file pointer an a counter of buckets (a.k.a. blocks) inside
     struct HashInstance{
-        FILE *hashingFile;
-        unsigned long buckets;
+        FILE *hashingFile;  //!< file pointer to the hashing file
+        unsigned long buckets; //!< buckets quantity in the current hash instance
 
+        /// Deafault constructor
         HashInstance();
 
+        /// Constructor that receives a path file and open the specified file, thus initializing the hashing file.
+        /// @author Tiago Santos
         explicit HashInstance(const char *path);
 
+        /// @author Tiago Santos
+        /// Closes the file pointer the hash instance
         void close();
     };
-
+     /**
+       * Initiates the hash file, allocating empty buckets. The quantity of buckets is determinated by the math:
+       * (numberOfRecord/recordsPerBucket) + 1. We also, can use a prime number to avoid collisions.
+       *
+       * @author Tiago Santos
+       * @param numberOfRecords Is used to determinate how many buckets the file must have.
+       * @param recordsPerBucket Also used to determinate how many buckets the file must have.
+       * @param path Path to the physical file that will keep the buckets.
+       */
     void createHash(unsigned long numberOfRecords, unsigned int recordsPerBucket, const char *path);
 
+     /**
+       * Initiates the overflow file, allocating just putting the header (quantity of blocks inside the file)
+       * that int this moment will be 0
+       *
+       * @author Tiago Santos
+       * @param numberOfRecords Is used to determinate how many buckets the file must have.
+       * @param recordsPerBucket Also used to determinate how many buckets the file must have.
+       * @param path Path to the physical file that will keep the buckets.
+       */
     void createOverflow(const char *path);
 
+     /**
+       * Inserts a record in the hash file and returns its address inside the data file.
+       *  Uses the hash function (see the implementation please) to get the mapped block and tries to alloc in there.
+       *  If succeeds, returns the allocated address immediately. If not, tries to alloc the record in the overflow area.
+       *
+       *
+       * @author Tiago Santos
+       * @param record Record that will be allocated.
+       * @param hash Current instance of the hashing.
+       * @param overflow Current instance of the overflow area.
+       */
     Address insertOnHashFile(Article record, HashInstance &hash, OverflowArea &overflow);
-
+     /**
+   *   Inserts a record in the overflow file. It's a recursive implementation.
+   *
+   * @author Tiago Santos
+   * @param record Record that will be allocated.
+   * @param overflowArea Current instance of the overflow area.
+   * @param offset Logical address (0,1,2,4,...) of the overflow block. Used to seek the block.
+   * @param needsUpdate Bool that indicates if the previous block in the logical linked list will need to be updated.
+   * @param address Address object passed by reference to keep the final address of the allocated record.
+   * @param alreadyLinked Indicates if a bucket/block already has a subsequent linked block.
+   */
     void insertOnOverflow(Article &record, OverflowArea &overflowArea, unsigned int &offset, bool &needsUpdate,
                           Address &address, bool alreadyLinked=false);
 
